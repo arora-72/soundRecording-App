4323513e00a4ee7a95b0920038594c66440c94f4
arora_72 <indresh.arora2016@vitstudent.ac.in>
2017-05-30 10:26:01 +0530

    Initial Commit

---
+++ soundRecording/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  soundRecording
+//
+//  Created by arora_72 on 30/05/17.
+//  Copyright © 2017 indresh arora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
